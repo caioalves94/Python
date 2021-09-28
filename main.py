945e10a7c079ac9d472667493518b298ce1ec45d
caioalves <c4iodark@gmail.com>
2021-09-28 12:08:24 -0300

    open web page(game)

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello, World!')
+import sys, os
+import subprocess as sp
+
+platform = sys.platform
+
+game = input('Você quer jogar um jogo? ')
+if (game == "sim"):
+    if ("win" in platform):
+        os.startfile("https://brunolemos.github.io/trust/")
+    elif ("linux" in platform):
+        try:
+            sp.Popen("https://brunolemos.github.io/trust/")
+        except FileNotFoundError:
+            sp.Popen("xdg-open", "https://brunolemos.github.io/trust/")
+
+
+
